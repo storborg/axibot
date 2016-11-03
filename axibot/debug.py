0e68af3bb9f6731424c7d50336b0569ac9da6fb9
Scott Torborg <storborg@gmail.com>
2016-11-02 21:22:04 -0700

    Start building out cornering planning features

--- axibot/debug.py
+++ axibot/debug.py
@@ -5,7 +5,14 @@ import argparse
 
 import matplotlib.pyplot as plt
 
-from axibot import svg
+from axibot import svg, planning, config
+
+
+def show(opts):
+    if opts.out:
+        plt.savefig(opts.out)
+    else:
+        plt.show()
 
 
 def debug_paths(opts):
@@ -23,7 +30,7 @@ def debug_paths(opts):
             ydata.append(-point.imag)
         plt.plot(xdata, ydata, 'g-')
 
-    plt.show()
+    show(opts)
 
 
 def debug_segments(opts):
@@ -43,7 +50,7 @@ def debug_segments(opts):
             ydata.append(-y)
 
     plt.plot(xdata, ydata, 'g-')
-    plt.show()
+    show(opts)
 
 
 def debug_transits(opts):
@@ -60,7 +67,48 @@ def debug_transits(opts):
             ydata.append(-y)
         plt.plot(xdata, ydata, 'r-' if pen_up else 'g-')
 
-    plt.show()
+    show(opts)
+
+
+def debug_corners(opts):
+    smoothness = 10
+    paths = svg.extract_paths(opts.filename)
+    segments = svg.plan_segments(paths, smoothness=smoothness)
+    transits = svg.add_pen_transits(segments)
+    points = planning.plan_velocity(transits)
+
+    up_xdata = []
+    up_ydata = []
+    up_speed = []
+    down_xdata = []
+    down_ydata = []
+    down_speed = []
+
+    def speed_to_color(speed, pen_up):
+        if pen_up:
+            alpha = 1 - (speed / config.SPEED_PEN_UP)
+            alpha = max(0, alpha)
+            return (1.0, 0.0, 0.0, alpha)
+        else:
+            alpha = 1 - (speed / config.SPEED_PEN_DOWN)
+            alpha = max(0, alpha)
+            return (0.0, 1.0, 0.0, alpha)
+
+    for point, pen_up, speed_limit in points:
+        x, y = point
+        y = -y
+        if pen_up:
+            up_xdata.append(x)
+            up_ydata.append(y)
+            up_speed.append(speed_to_color(speed_limit, pen_up))
+        else:
+            down_xdata.append(x)
+            down_ydata.append(y)
+            down_speed.append(speed_to_color(speed_limit, pen_up))
+    plt.scatter(up_xdata, up_ydata, s=100, linewidths=1, c=up_speed)
+    plt.scatter(down_xdata, down_ydata, s=100, linewidths=1, c=down_speed)
+
+    show(opts)
 
 
 def main(argv=sys.argv):
@@ -73,17 +121,26 @@ def main(argv=sys.argv):
     p_paths = subparsers.add_parser(
         'paths', help='Render normalized paths.')
     p_paths.add_argument('filename')
+    p_paths.add_argument('--out')
     p_paths.set_defaults(function=debug_paths)
 
     p_segments = subparsers.add_parser(
         'segments', help='Render linear segments.')
     p_segments.add_argument('filename')
+    p_segments.add_argument('--out')
     p_segments.set_defaults(function=debug_segments)
 
     p_transits = subparsers.add_parser(
         'transits', help='Render segments with pen transits.')
     p_transits.add_argument('filename')
+    p_transits.add_argument('--out')
     p_transits.set_defaults(function=debug_transits)
+
+    p_corners = subparsers.add_parser(
+        'corners', help='Render points with speeds.')
+    p_corners.add_argument('filename')
+    p_corners.add_argument('--out')
+    p_corners.set_defaults(function=debug_corners)
 
     opts, args = p.parse_known_args(argv[1:])
 
--- axibot/planning.py
+++ axibot/planning.py
@@ -562,3 +562,56 @@ def plan_trajectory(path, pen_up):
                                                   traj_velocities[i],
                                                   pen_up=pen_up))
     return actions
+
+
+def cornering_angle(a, b, c):
+    """
+    Given three points, compute the angle in radians between AB and BC.
+    """
+    ax, ay = a
+    bx, by = b
+    cx, cy = c
+    return abs(math.atan2(ay - by, ax - bx) - math.atan2(cy - by, cx - bx))
+
+
+def cornering_velocity(angle, pen_up):
+    """
+    Given a corner angle in radians, compute the desired cornering velocity.
+
+    An angle of pi can go full speed. An angle of zero needs to fully changeh
+    direction, so it needs to have zero velocity. Everywhere in between...?
+
+    XXX figure this out
+    """
+    ratio = angle / math.pi
+    max_speed = config.SPEED_PEN_UP if pen_up else config.SPEED_PEN_DOWN
+    return ratio * max_speed
+
+
+def segment_corner_limits(segment, pen_up):
+    """
+    Given a segment and pen state, tag each point with the 'speed limit' for
+    that corner.
+    """
+    out = []
+    out.append((segment[0], 0.0))
+
+    for a, b, c in zip(segment[:-2], segment[1:-1], segment[2:]):
+        angle = cornering_angle(a, b, c)
+        limit = cornering_velocity(angle, pen_up)
+        out.append((b, limit))
+
+    out.append((segment[-1], 0.0))
+    return out
+
+
+def plan_velocity(transits):
+    """
+    Given a list of (segment, pen_up) tuples, return a list of
+    (point, pen_up, speed_limit) tuples.
+    """
+    points = []
+    for segment, pen_up in transits:
+        for point, speed_limit in segment_corner_limits(segment, pen_up):
+            points.append((point, pen_up, speed_limit))
+    return points
