144626040bbf32a6dac2974e192d4737ef0ba506
Scott Torborg <storborg@gmail.com>
2016-11-04 09:27:29 -0700

    Build out acceleration/deceleration planning

--- axibot/planning.py
+++ axibot/planning.py
@@ -63,16 +63,80 @@ def plan_speed_limits(transits):
     return out
 
 
-def plan_segment_profile(start, end, vstart, vmax, pen_up):
+def plan_acceleration(start, end, v_start, vmax_end, pen_up):
     """
     Given start and end points, start and end speed limits, and the pen state
     for this line, compute the desired velocity profile along the line.
+
+    Return the list of moves.
+
+    Also return the actual final speed, so that it can be used as the initial
+    speed for the start of the next segment.
+
+    Velocity profile can either be:
+        - trapezoidal (accelerate, constant, decelerate)
+        - triangular (accelerate, decelerate)
+        - linear (accelerate OR decelerate)
+
+    Begin by computing the required acceleration time from vmax_start to the
+    max speed and the deceleration time from max speed to vmax_end.
     """
+    vmax_all = config.SPEED_PEN_UP if pen_up else config.SPEED_PEN_DOWN
+    accel_rate = vmax_all / (config.ACCEL_TIME_PEN_UP
+                             if pen_up else
+                             config.ACCEL_TIME_PEN_DOWN)
+    assert vmax_start <= vmax_all
+    assert vmax_end <= vmax_all
+
+    dist = math.sqrt((end[0] - start[0])**2 + (end[1] - start[1])**2)
+
+    accel_time = (vmax_all - vmax_start) / accel_rate
+    decel_time = (vmax_all - vmax_end) / accel_rate
+    accel_dist = (vmax_start * accel_time) + (0.5 * accel_rate * (t_accel**2))
+    decel_dist = (vmax_end * decel_time) + (0.5 * accel_rate * (t_decel**2))
+
+    timeslice = config.TIME_SLICE
+
+    if dist > (accel_dist + decel_dist + (timeslice * vmax_all)):
+        # Trapezoidal velocity profile.
+        pass
+    elif dist > accel_dist:
+        # Triangular
+        # We have enough time to accelerate to full speed, but not decelerate.
+        # So instead we'll just go as fast as we can and still have enough
+        # deceleration time.
+        pass
+    else:
+        # Linear
+        # We can't even reach the max speed anywhere in this segment. So
+        # we'll just accelerate to vmax_end, if we can.
+        pass
+
+    return v, actions
 
 
 def plan_moves(segments_limits):
     """
     Given a list of (start, end, vmax_start, vmax_end, pen_up) tuples, return a
     list of moves, with acceleration and deceleration plotted between points.
+    Also add any pen state changes.
     """
-    pass
+    v_last = 0
+    pen_up_last = False
+
+    actions = []
+
+    for start, end, vmax_start, vmax_end, pen_up in segments_limits:
+        if pen_up != pen_up_last:
+            # Add pen move
+            if pen_up:
+                # XXX get the right delay
+                actions.append(moves.PenUpMove(1000))
+            else:
+                # XXX get the right delay
+                actions.append(moves.PenDownMove(1000))
+        assert v_last <= vmax_start
+        v_last, seg_actions = plan_acceleration(start, end, v_last, vmax_end, pen_up)
+        actions.extend(seg_actions)
+
+    return actions
