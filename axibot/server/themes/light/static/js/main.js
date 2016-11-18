23a3d79c7170c80507882367790c4da5714560c0
Scott Torborg <storborg@gmail.com>
2016-11-18 15:24:35 -0700

    Server: rip out dropzone.js

--- axibot/server/themes/light/static/js/main.js
+++ axibot/server/themes/light/static/js/main.js
@@ -1,76 +1,10 @@
 require([
   'jquery',
-  'dropzone',
   'utils'
-], function ($, Dropzone, utils) {
-
-    // Extended disable function
-    jQuery.fn.extend({
-        disable: function(state) {
-            return this.each(function() {
-                var $this = $(this);
-                if($this.is('input, button, textarea, select'))
-                    this.disabled = state;
-                else
-                    $this.toggleClass('disabled', state);
-            });
-        }
-    });
-
-    function initDropZone() {
-      Dropzone.options.dropper = {
-          init: function() {
-              this.on("thumbnail", function () {
-                  // XXX: is there a better way to do this?
-                  var width = $(".dz-image img").width();
-                  $("#dropper").width(width);
-              });
-          },
-          paramName: "file", // The name that will be used to transfer the file
-          autoProcessQueue: false, // Don't auto upload files
-          autoDiscover: "false",
-          accept: function(file, done) {
-              console.log("dropzone accepted file");
-              console.log(file);
-              done();
-          }
-      };
-
-      var dropper = new Dropzone("div#dropper",
-          { url: "/upload", // Set url to something random to avoid auto uploads
-            addRemoveLinks: true} // Show remove button
-      );
-      // Make the dropper green when a file is over it
-      dropper.on("dragover", function () {
-          console.log("setting hover class");
-          this.className = 'hover'; return false;
-      });
-      dropper.on("dragleave",  function () {
-          console.log("removing hover");
-          this.className = ''; return false;
-      });
-      dropper.on("addedfile", function() {
-          console.log("Enabling start");
-          $("#start").disable(false);
-      });
-      dropper.on("removedfile", function() {
-          console.log("Disabling start");
-          $("#start").disable(true);
-      });
-      return dropper;
-  }
-
-  function bindButtons(dropper) {
-      $("#start").on("click", function() {
-          dropper.processQueue();
-      })
-  }
+], function ($, utils) {
 
   $(function () {
     console.log('Loaded light: main.');
-
-    var dropper = initDropZone();
-    bindButtons(dropper);
 
     var sock = new WebSocket(utils.qualifyWebsocketURL("/api"));
     sock.onopen = function () {
