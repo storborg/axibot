a1ac4bb9ae3519795cffedbeb7ee52a40bf0990b
Scott Torborg <storborg@gmail.com>
2016-11-18 17:38:15 -0700

    Server: start to vue-ify

--- axibot/server/themes/light/static/js/main.js
+++ axibot/server/themes/light/static/js/main.js
@@ -1,32 +1,52 @@
 require([
-  'jquery',
+  'vue',
   'utils'
-], function ($, utils) {
+], function (Vue, utils) {
 
-  $(function () {
-    console.log('Loaded light: main.');
-
-    var sock = new WebSocket(utils.qualifyWebsocketURL("/api"));
-    sock.onopen = function () {
-      console.log("websocket: opened");
-    }
-
-    sock.onerror = function (error) {
-      console.log("websocket: error", error);
-    }
-
-    sock.onmessage = function (e) {
-      var msg = JSON.parse(e.data);
-      console.log("websocket: message", msg);
-
-      if (msg.type === 'state') {
-        $('#state').text(msg.state);
-        $('#num_actions').text(msg.num_actions);
-        $('#action_index').text(msg.action_index);
+  var vm = new Vue({
+    el: '#app',
+    data: {
+      'state': 'error',
+      'actionIndex': 0,
+      'numActions': 0
+    },
+    methods: {
+      penUp: function () {
+        console.log("pen up pressed");
+      },
+      penDown: function () {
+        console.log("pen down pressed");
+      },
+      startPlotting: function () {
+        console.log("start plotting");
+      },
+      pausePlotting: function () {
+        console.log("pause plotting");
+      },
+      cancelPlotting: function () {
+        console.log("cancel plotting");
       }
-
     }
-
   });
+
+  var sock = new WebSocket(utils.qualifyWebsocketURL("/api"));
+  sock.onopen = function () {
+    console.log("websocket: opened");
+  }
+
+  sock.onerror = function (error) {
+    console.log("websocket: error", error);
+  }
+
+  sock.onmessage = function (e) {
+    var msg = JSON.parse(e.data);
+    console.log("websocket: message", msg);
+
+    if (msg.type === 'state') {
+      vm.state = msg.state;
+      vm.numActions = msg.num_actions;
+      vm.actionIndex = msg.action_index;
+    }
+  }
 });
 
