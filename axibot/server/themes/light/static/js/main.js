6f25f286a43b5fe7e82f91d27a6bd3be0da2dc73
Scott Torborg <storborg@gmail.com>
2016-11-22 01:00:13 -0800

    Server: clean up vue app and somewhat improve scale of preview pen

--- axibot/server/themes/light/static/js/main.js
+++ axibot/server/themes/light/static/js/main.js
@@ -3,64 +3,6 @@ require([
   'utils'
 ], function (Vue, utils) {
 
-  var sock = new WebSocket(utils.qualifyWebsocketURL("/api"));
-
-  sock.onerror = function (error) {
-    alert("websocket error: " + error);
-  }
-
-  sock.onmessage = function (e) {
-    var msg = JSON.parse(e.data);
-
-    if (msg.type === 'state') {
-      vm.state = msg.state;
-      vm.numActions = msg.num_actions;
-      vm.actionIndex = msg.action_index;
-      vm.penX = msg.x;
-      vm.penY = msg.y;
-      // XXX This should be done with bound attributes and scaled properly
-      setPen(vm.penX / 20, vm.penY / 20);
-
-    } else if (msg.type == 'new-document') {
-      var doc = document.getElementById('document');
-      doc.innerHTML = msg.document;
-
-    } else if (msg.type == 'error') {
-      alert("Server Error: " + msg.text);
-
-    }
-  }
-
-  function setPen(x, y) {
-    console.log("setpen", x, y);
-    var pen = document.getElementById('pen');
-    pen.style.left = x + 'px';
-    pen.style.top = y + 'px';
-  }
-
-  function sendMessage(msg) {
-    sock.send(JSON.stringify(msg));
-  }
-
-  function sendFile(doc) {
-    var msg = {
-      type: 'set-document',
-      document: doc
-    };
-    sendMessage(msg);
-  }
-
-  function handleFile(file) {
-      // Set the contents of the preview image to this doc and send msg
-      var reader = new FileReader();
-      var doc = document.getElementById('document');
-      reader.onload = function (e) {
-        doc.innerHTML = e.target.result;
-        sendFile(e.target.result);
-      }
-      reader.readAsText(file);
-  }
-
   var vm = new Vue({
     el: '#app',
     data: {
@@ -82,28 +24,80 @@ require([
       },
       disableCancelPlotting: function () {
         return (this.state != 'plotting') && (this.state != 'paused');
+      },
+      previewX: function () {
+        return this.penX / 2032;
+      },
+      previewY: function () {
+        return this.penY / 2032;
       }
     },
     methods: {
       penUp: function () {
-        sendMessage({type: "manual-pen-up"});
+        this.sendMessage({type: "manual-pen-up"});
       },
       penDown: function () {
-        sendMessage({type: "manual-pen-down"});
+        this.sendMessage({type: "manual-pen-down"});
       },
       resumePlotting: function () {
-        sendMessage({type: "resume-plotting"});
+        this.sendMessage({type: "resume-plotting"});
       },
       pausePlotting: function () {
-        sendMessage({type: "pause-plotting"});
+        this.sendMessage({type: "pause-plotting"});
       },
       cancelPlotting: function () {
-        sendMessage({type: "cancel-plotting"});
+        this.sendMessage({type: "cancel-plotting"});
       },
       fileSelected: function (e) {
         if (e.target.files.length > 0) {
           console.log("file selected", e.target.files[0]);
-          handleFile(e.target.files[0]);
+          this.handleFile(e.target.files[0]);
+        }
+      },
+      sendMessage: function (msg) {
+        this.sock.send(JSON.stringify(msg));
+      },
+      sendFile: function (doc) {
+        var msg = {
+          type: 'set-document',
+          document: doc
+        };
+        this.sendMessage(msg);
+      },
+      handleFile: function (file) {
+          // Set the contents of the preview image to this doc and send msg
+          var reader = new FileReader();
+          var doc = document.getElementById('document');
+          reader.onload = function (e) {
+            doc.innerHTML = e.target.result;
+            this.sendFile(e.target.result);
+          }
+          reader.readAsText(file);
+      }
+    },
+    created: function () {
+      this.sock = new WebSocket(utils.qualifyWebsocketURL("/api"));
+      this.sock.onerror = function (error) {
+        alert("websocket error: " + error);
+      }
+      this.sock.onmessage = function (e) {
+        var msg = JSON.parse(e.data);
+
+        if (msg.type === 'state') {
+          vm.state = msg.state;
+          vm.numActions = msg.num_actions;
+          vm.actionIndex = msg.action_index;
+          // XXX properly scale this to document preview scale
+          vm.penX = msg.x;
+          vm.penY = msg.y;
+
+        } else if (msg.type == 'new-document') {
+          var doc = document.getElementById('document');
+          doc.innerHTML = msg.document;
+
+        } else if (msg.type == 'error') {
+          alert("Server Error: " + msg.text);
+
         }
       }
     }
