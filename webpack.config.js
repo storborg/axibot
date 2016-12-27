0802767aca924d83cba510d8a16cb3df1287fc77
Scott Torborg <storborg@gmail.com>
2016-12-26 23:44:18 -0800

    Server: add production configuration for webpack, so that a more modest file is generated

--- webpack.config.js
+++ webpack.config.js
@@ -1,4 +1,43 @@
+const webpack = require('webpack');
+
+const nodeEnv = process.env.NODE_ENV || 'development';
+const isProd = nodeEnv == 'production';
+
+const plugins = [
+  new webpack.DefinePlugin({
+    'process.env': { NODE_ENV: JSON.stringify(nodeEnv) }
+  }),
+];
+
+if (isProd) {
+  plugins.push(
+    new webpack.NamedModulesPlugin(),
+    new webpack.LoaderOptionsPlugin({
+      minimize: true,
+      debug: false
+    }),
+    new webpack.optimize.UglifyJsPlugin({
+      compress: {
+        warnings: false,
+        screw_ie8: true,
+        conditionals: true,
+        unused: true,
+        comparisons: true,
+        sequences: true,
+        dead_code: true,
+        evaluate: true,
+        if_return: true,
+        join_vars: true
+      },
+      output: {
+        comments: false
+      }
+    })
+  );
+}
+
 module.exports = {
+  devtool: isProd ? 'source-map' : 'eval',
   entry: [
     './axibot/server/js/app.js',
     './axibot/server/css/app.scss'
@@ -11,6 +50,7 @@ module.exports = {
       'vue$': 'vue/dist/vue.common.js'
     }
   },
+  plugins,
   module: {
     rules: [
       {
