43385f2762d032b75083b0024e2a28acbaa476fc
Scott Torborg <storborg@gmail.com>
2019-03-02 10:52:16 -0800

    Update version to 0.0.3 for release

--- setup.py
+++ setup.py
@@ -4,7 +4,7 @@ from setuptools import setup, find_packages
 
 
 setup(name='axibot',
-      version='0.0.3.dev',
+      version='0.0.3',
       description='Software for AxiDraw pen plotting robot',
       long_description='',
       classifiers=[
